60587f3f36f600d7b857b1850479a242e33846c4
Douglas Mendes <douglasleomendes@hotmail.com>
2024-12-20 12:38:34 +0000

    fix: cypres tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,10 @@
-
-import { defineConfig } from 'cypress'
-
-module.exports = {
-    projectId: "vntsqg",
-    // ...rest of the Cypress project config
-  }
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: "vntsqg",
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
-})
+});

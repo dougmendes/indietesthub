557ed480bbb2057ad3aa484bdd07028a97e3b9c6
Douglas Mendes <douglasleomendes@hotmail.com>
2024-12-23 19:12:36 +0000

    add some dummy config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   projectId: "vntsqg",
   e2e: {
-    setupNodeEvents() {
+    setupNodeEvents(on, config) {
+      // Dummy usage to satisfy ESLint
+      console.log('Setup Node Events initialized');
+      console.log(on, config);
+      
       // implement node event listeners here
     },
   },

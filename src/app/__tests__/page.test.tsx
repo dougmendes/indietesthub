f1ffa54662e4d46d21c2d17def5b421944e327de
Douglas Mendes <douglasleomendes@hotmail.com>
2024-12-23 19:29:20 +0000

    add some lint conf

--- src/app/__tests__/page.test.tsx
+++ src/app/__tests__/page.test.tsx
@@ -4,10 +4,10 @@ import Home from '../page'
 test('renders header and navigation links', () => {
   render(<Home />);
 
-  expect(screen.getByRole('heading', { level: 1, name: 'We are |' })).toBeDefined()
-  expect(screen.getByRole('button', {  name: 'Login' })).toBeDefined()
-  expect(screen.getByRole('button', {  name: 'Sign up' })).toBeDefined()
-  expect(screen.getByRole('link', {  name: 'Testers' })).toBeDefined()
-  expect(screen.getByRole('link', {  name: 'Projects' })).toBeDefined()
-  expect(screen.getByRole('link', {  name: 'Company' })).toBeDefined()
+  expect(screen.getByRole('heading', { level: 1, name: 'We are |' }))
+  expect(screen.getByRole('button', {  name: 'Login' }))
+  expect(screen.getByRole('button', {  name: 'Sign up' }))
+  expect(screen.getByRole('link', {  name: 'Testers' }))
+  expect(screen.getByRole('link', {  name: 'Projects' }))
+  expect(screen.getByRole('link', {  name: 'Company' }))
 })

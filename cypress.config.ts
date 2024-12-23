0bb96b1b30682972ab62dc1b6e8cdaf96a5b8371
Douglas Mendes <douglasleomendes@hotmail.com>
2024-12-23 19:08:14 +0000

    fix some eslint rules

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   projectId: "vntsqg",
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents() {
       // implement node event listeners here
     },
   },
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./styles/globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next" // eslint-disable-line no-use-before-define
+import { SpeedInsights } from "@vercel/speed-insights/next" 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
